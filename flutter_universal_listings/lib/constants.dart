5c899f4f9b4679d1bb012ef19c138589e9400775
me <21152260+saulkim@users.noreply.github.com>
2023-09-10 18:01:04 -0700

    unknown keys

--- flutter_universal_listings/lib/constants.dart
+++ flutter_universal_listings/lib/constants.dart
@@ -6,8 +6,9 @@ const chatChannelsCollection = 'channels';
 const messagesLiveCollection = 'messages_live';
 const pageSizeLimit = 20;
 const liveCollectionLimit = 50;
+// ?
 const serverKey =
     'AAAAZz4RcfQ:APA91bEljtQ-DRJv3ZIXNR__lrY3m9puCSx9GeHs619bqhVDSwzo1tnXSZEYU0eMN1WNOvoeTx6Mkc_vSEaoJ65HCf_1BKXnGHQA9dwF3II4rH9I3YvUkWYCW2ocyGWqLS-pUYIr_ux-';
 const eula = 'https://www.instamobile.io/eula-instachatty/';
 const privacyPolicyURL = 'https://instamobile.io/privacy-policy/';
-const googleApiKey = 'AIzaSyB_9oADHEV8hjbJrq0-790p4pZdFD8Rgxo';
+const googleApiKey = 'AIzaSyB_9oADHEV8hjbJrq0-790p4pZdFD8Rgxo'; // ?
